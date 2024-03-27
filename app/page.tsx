7fff4b17f2bfcb4cd9058bdc4b6822ee552f8a92
ngokturkkarli <ngokturkkarli@gmail.com>
2024-03-27 20:36:46 +0300

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <Button>Test</Button>
+    </main>
+  );
+}
